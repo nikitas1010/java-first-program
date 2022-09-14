94ef4f52777ae0fa6e8fa46ddd2c3a4eea26c0ae
nikitas1010 <sinhal.nikita@gmail.com>
2022-09-14 21:08:17 +0800

    loan rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,15 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
     }
+
+    private static int add(int[] number) {
+        var sum = 0;
+        for (int each_number : number ){
+            sum +=each_number;
+        }
+        return sum;
+    }
+
 }
